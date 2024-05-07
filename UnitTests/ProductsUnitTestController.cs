0f2000317fae927600eca0ce1e5d6acf71ecebdc
Giovani de Oliveira <giovani98@proton.me>
2024-05-07 14:29:21 -0300

    Setting up xUnit project

---
+++ UnitTests/ProductsUnitTestController.cs
@@ -0,0 +1,52 @@
+﻿using AutoMapper;
+using CatalogAPI.Context;
+using CatalogAPI.DTOs.Mappings;
+using CatalogAPI.Repositories;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CatalogAPIxUnitTests.UnitTests
+{
+    internal class ProductsUnitTestController
+    {
+        // Definição de variáveis
+        private IUnitOfWork repository;
+        private IMapper mapper;
+
+        public static DbContextOptions<AppDbContext> dbContextOptions { get; }
+
+        // Connection string
+        public static string connectionString = 
+            "Server=localhost;Database=catalogdb;Uid=root;Pwd=1234";
+
+        // Construtores
+        static ProductsUnitTestController()
+        {
+            dbContextOptions = new DbContextOptionsBuilder<AppDbContext>()
+                .UseMySql(connectionString, ServerVersion.AutoDetect(connectionString))
+                .Options;
+        }
+
+        // Construtor padrão
+        public ProductsUnitTestController()
+        {
+            // Mapper setup
+            var config = new MapperConfiguration(config =>
+            {
+                config.AddProfile(new ProductDTOMappingProfile());
+            });
+            mapper = config.CreateMapper();
+
+            // Context setup
+            var context = new AppDbContext(dbContextOptions);
+
+            // Repository setup
+            repository = new UnitOfWork(context);
+        }
+
+    }
+}
--- obj/Debug/net8.0/CatalogAPIxUnitTests.AssemblyInfo.cs
+++ obj/Debug/net8.0/CatalogAPIxUnitTests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CatalogAPIxUnitTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c000760bd26bee74ad5e9d77e43095aaba0538fe")]
 [assembly: System.Reflection.AssemblyProductAttribute("CatalogAPIxUnitTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CatalogAPIxUnitTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
