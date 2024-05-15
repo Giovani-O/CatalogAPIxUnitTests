1d8cd191223a345101d7ffe7eb8e784690d843dc
Giovani de Oliveira <giovani98@proton.me>
2024-05-15 13:51:57 -0300

    fix: Remove unnecessary auth policies

--- obj/Release/net8.0/CatalogAPIxUnitTests.AssemblyInfo.cs
+++ obj/Release/net8.0/CatalogAPIxUnitTests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CatalogAPIxUnitTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e15cbe328688d224483a834703926baf55eb5f8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5101bdd7385de511a3d223f3649dcba011edf7eb")]
 [assembly: System.Reflection.AssemblyProductAttribute("CatalogAPIxUnitTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CatalogAPIxUnitTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
